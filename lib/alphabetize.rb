a6cc74c9f005a63a9df0bfb91fbe17c01ec781bf
Malcolm Leake <echelon1089@gmail.com>
2018-11-19 23:13:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,11 @@
+require 'pry'
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(sentences)
   sentences.sort_by do |sentence|
     sentence.split("").map do |letter|
       ALPHABET.index(letter)
+   binding.pry
     end
   end
 end
